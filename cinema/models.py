2f46eef5245fe2ae05b46292146164536859ae77
Artur Bashyrov <bashyroov@gmail.com>
2025-10-15 16:41:44 +0200

    Fix flake8 and add actors,genres fields to Movie Serializer

--- cinema/models.py
+++ cinema/models.py
@@ -5,13 +5,16 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
 
+
 class Genre(models.Model):
     name = models.CharField(max_length=64, unique=True)
+
 
 class CinemaHall(models.Model):
     name = models.CharField(max_length=64)
     rows = models.IntegerField()
     seats_in_row = models.IntegerField()
+
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
--- cinema/serializers.py
+++ cinema/serializers.py
@@ -6,6 +6,14 @@ from cinema.models import Movie, Genre, Actor, CinemaHall
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(max_length=255)
+    actors = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Actor.objects.all()
+    )
+    genres = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Genre.objects.all()
+    )
     description = serializers.CharField()
     duration = serializers.IntegerField()
 
@@ -23,15 +31,18 @@ class MovieSerializer(serializers.Serializer):
 
         return instance
 
+
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
         fields = "__all__"
 
+
 class ActorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Actor
         fields = "__all__"
+
 
 class CinemaHallSerializer(serializers.ModelSerializer):
     class Meta:
